32c97842454c36c8ddbf3660bcb6f5b8fc2dd82b
masifmirza927 <masifmirza927@gmail.com>
2023-06-22 16:39:48 +0500

    image uploading

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,31 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Create() {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [address, setAddress] = useState("");
+const [about, setAbout] = useState("");
+
+const handleSubmit = () => {
+  // validate values
+
+  console.log(name, email, address, about)
+
+
+  // create formData
+  // send data to server
+
+
+}
+
   return (
     <>
-      <div style={{ width: "600px" }} className='mx-auto'>
-        <div className="mb-3">
-          <label htmlFor="formFile" class="form-label">Image</label>
-          <input className="form-control" type="file" id="formFile" />
+      <form onSubmit={ (event) => { event.preventDefault() } }>
+        <div style={{ width: "600px" }} className='mx-auto'>
+          <div className="mb-3">
+            <label htmlFor="formFile" className="form-label">Image</label>
+            <input className="form-control" type="file" id="formFile" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input onChange={ (event) => { setName(event.target.value) } }  type="text" className="form-control" id="name" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">Address</label>
+            <input type="text" className="form-control" id="address" />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">About</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button onClick={handleSubmit} className='btn btn-primary'>Create</button>
         </div>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="address" className="form-label">Address</label>
-          <input type="text" className="form-control" id="address" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">About</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <button className='btn btn-primary'>Create</button>
-      </div>
+      </form>
+
     </>
   )
 }
