1d5bd3cde3b20721aff01f82402b7fca73f04c5a
masifmirza927 <masifmirza927@gmail.com>
2023-06-23 16:51:36 +0500

    student details

--- src/pages/StudentDetail.js
+++ src/pages/StudentDetail.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import axios from "axios";
 
 
@@ -19,9 +19,25 @@ function StudentDetail() {
     if (student != null) {
         return (
             <>
-                {
-                    student.name
-                }
+                <div className="card mb-3">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={student.image} className="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">{student.name}</h5>
+                                <p className="card-text">{student.email}</p>
+                                <p className="card-text">{student.address}</p>
+                                <p className="card-text">{student.about}</p>
+                            </div>
+                            <div>
+                                <Link to="/" className="btn btn-info mx-2">Edit</Link>
+                                <button className="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
