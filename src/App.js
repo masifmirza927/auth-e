bda07cbe545b889f469e4f138c0c7ae1850bd093
masifmirza927 <masifmirza927@gmail.com>
2023-06-23 17:36:18 +0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -5,31 +5,15 @@ import Home from "./pages/Home"
 import Create from "./pages/Create"
 import StudentDetail from "./pages/StudentDetail"
 import Search from './pages/Search';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 function App() {
-  const [students, setStudents] = useState([]);
-
-useEffect( () => {
-
-  axios.get("http://localhost:3003/students").then( (res) => {
-    console.log(res.data);
-    if(res.data.status == true) {
-      console.log(res.data.students)
-      setStudents(res.data.students);
-    }
-  })
-
-
- }, [] );
 
   return (
     <div className='container'>
       <Navbar />
 
     <Routes>
-      <Route path='/' element={<Home students={students} />} />
+      <Route path='/' element={<Home />} />
       <Route path="/create" element={<Create />} />
       <Route path='/search' element={<Search />} />
       <Route path='/student/:id' element={<StudentDetail />} /> 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,50 @@
-import {Link} from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import axios from 'axios';
 
-function Home(props) {
+function Home() {
 
-    if (props.students.length < 0) {
-        return (
-          <><div className="spinner-border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div></>
-        )
-      } else {
-        return (
-          <>
-            <div className='row'>
-              {
-                props.students.map((student) => {
-                  return (
-                    <div className='col-md-3' key={student._id}>
-                      <div className="card" style={{ width: "18rem", marginBottom: "20px" }} >
-                        <img src={student.image} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h5 className="card-title">{student.name}</h5>
-                          <Link to={`/student/${student._id}`} className="btn btn-primary">View</Link>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-    
-          </>
-        )
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+
+    axios.get("http://localhost:3003/students").then((res) => {
+      if (res.data.status == true) {
+        setStudents(res.data.students);
       }
+    })
+  }, []);
+
+  if (students.length < 0) {
+    return (
+      <><div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div></>
+    )
+  } else {
+    return (
+      <>
+        <div className='row'>
+          {
+            students.map((student) => {
+              return (
+                <div className='col-md-3' key={student._id}>
+                  <div className="card" style={{ width: "18rem", marginBottom: "20px" }} >
+                    <img src={student.image} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">{student.name}</h5>
+                      <Link to={`/student/${student._id}`} className="btn btn-primary">View</Link>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+
+      </>
+    )
+  }
 }
 
 export default Home
